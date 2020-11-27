9ed4beca5747da489679b9778bdeb28c3c1fe9bb
Fabbelevy <fabricelindblom@gmail.com>
2020-11-27 12:06:29 +0100

    added "swing" design class

---
+++ application/IU6App.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class IU6App {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IU6App window = new IU6App();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public IU6App() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
